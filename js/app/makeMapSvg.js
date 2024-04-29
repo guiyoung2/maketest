bb12f03c11b16e2e0b89ba2afd59ea53e98fa386
guiyoung <guiyoung22@naver.com>
2024-04-29 10:11:53 +0900

    mobileplz

--- js/app/makeMapSvg.js
+++ js/app/makeMapSvg.js
@@ -6,45 +6,7 @@ $(function () {
     $(this).makeMapSvg(option);
   });
 });
-//  // 모바일 일 때 비활성화 
-//  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-//   // 여기에 모바일 장치에서 실행할 코드 작성
-//   // $(".select-box .list-box .btn-file").css("background","#EAEAEA")
-//   // $(".select-box .list-box .btn-file").css("color","#cccccc")
-//   // $(".select-box .list-box .btn-file").css("pointer-events","none")
-//   // $(".aside-button-cont .btn-file").css("color","#cccccc")
-//   // $(".aside-button-cont .btn-file .rollout").attr("src","./images/drawTool/button_file_none.png")
-//   // $(".aside-button-cont .btn-file").css("pointer-events","none")
-//   // $(".right-cont .btn-print").css("background","url(../images/btn_save_none.png) no-repeat center center;")
-//   // $(".right-cont .btn-print").css("pointer-events","none")
-//   alert("모바일!!!!!!")
-//   $(".select-box .list-box .btn-file").css({
-//     "background": "#EAEAEA",
-//     "color": "#cccccc",
-//     "pointer-events": "none"
-//   }); 
-//   $(".aside-button-cont .btn-file").css({
-//     "color": "#cccccc",
-//     "pointer-events": "none"
-//   }); 
-//   $(".aside-button-cont .btn-file .rollout").attr("src","./images/drawTool/button_file_none.png")
-//   $(".right-cont .btn-print").css("background","url(../images/btn_save_none.png) no-repeat center center;")
-//   $(".right-cont .btn-print").css("pointer-events","none")
-// } else {
-//   alert("컴퓨터!!")
-//   $(".select-box .list-box .btn-file").css({
-//     "background": "#ffffff",
-//     "color": "#000000",
-//     "pointer-events": "all"
-//   }); 
-//   $(".aside-button-cont .btn-file").css({
-//     "color": "#000000",
-//     "pointer-events": "all"
-//   }); 
-//   $(".aside-button-cont .btn-file .rollout").attr("src","./images/drawTool/button_file_over.png")
-//   $(".right-cont .btn-print").css("background","url(../images/btn_save.png) no-repeat center center;")
-//   $(".right-cont .btn-print").css("pointer-events","all")
-// }
+
 /*
  *	MakeMapSvg :: require (raphael.js, raphael.free_transform.js, html2canvas.js)
  */
@@ -116,17 +78,13 @@ var undoData; // 임시 되돌리기 배열
           }
         });
         // // 모바일 일 때 비활성화 
-        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        var agent = navigator.userAgent.toLowerCase();
+        var safariException = agent.indexOf("mac")
+        console.log(agent.indexOf("mac"));
+
+        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || (safariException && document.documentElement.clientWidth <= 1024)) {
           // 여기에 모바일 장치에서 실행할 코드 작성
-          // $(".select-box .list-box .btn-file").css("background","#EAEAEA")
-          // $(".select-box .list-box .btn-file").css("color","#cccccc")
-          // $(".select-box .list-box .btn-file").css("pointer-events","none")
-          // $(".aside-button-cont .btn-file").css("color","#cccccc")
-          // $(".aside-button-cont .btn-file .rollout").attr("src","./images/drawTool/button_file_none.png")
-          // $(".aside-button-cont .btn-file").css("pointer-events","none")
-          // $(".right-cont .btn-print").css("background","url(../images/btn_save_none.png) no-repeat center center;")
-          // $(".right-cont .btn-print").css("pointer-events","none")
-          alert("모바일!!!!!!")
+          console.log("모바일");
           $(".select-box .list-box .btn-file").css({
             "background": "#EAEAEA",
             "color": "#cccccc",
@@ -137,12 +95,12 @@ var undoData; // 임시 되돌리기 배열
             "pointer-events": "none"
           }); 
           $(".aside-button-cont .btn-file .rollout").attr("src","./images/drawTool/button_file_none.png")
-          $(".right-cont .btn-print").css("background","url(./images/btn_print_none.png) no-repeat center center;")
+          $(".right-cont .btn-print").css("background","url(./images/btn_print_none.png)")
           $(".right-cont .btn-print").css("width","52px")
           $(".right-cont .btn-print").css("height","52px")
           $(".right-cont .btn-print").css("pointer-events","none")
         } else {
-          alert("컴퓨터!!")
+          console.log("pc");
           $(".select-box .list-box .btn-file").css({
             "background": "#ffffff",
             "color": "#000000",
@@ -159,27 +117,6 @@ var undoData; // 임시 되돌리기 배열
           $(".right-cont .btn-print").css("pointer-events","all")
         }
 
-        var agent = navigator.userAgent.toLowerCase();
-if ( navigator.userAgent.match(/iPhone|iPad|iPod/i) != null ) {
-    if( agent.indexOf("crios") == -1 ) {
-        alert("safari");
-    }else{
-        alert("chrome");
-    }
-}
-        
-            // // 사용자 에이전트 문자열 가져오기
-            // var isMobile = /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|Tablet|iPad/i.test(navigator.userAgent);
-            // jQuery(document).ready(function($){
-            //      if(isMobile) {
-            //           //PC
-            //           alert("모바일")
-            //         } else {
-            //           //MOBILE
-            //           alert("컴퓨터")
-            //      }
-            // });
-            
             
         // 스크롤바 디자인 적용
         $('.scroll-box').each(function () {
